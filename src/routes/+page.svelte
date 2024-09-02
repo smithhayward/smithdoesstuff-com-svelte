aa462fcdc8814851e65a7e872d227d770528979b
Smith Hayward <smith.hayward@gmail.com>
2024-09-02 15:07:09 -0700

    commit to feature-1234

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+    import { writable } from 'svelte/store';
+    
+    import { fade, slide, fly } from 'svelte/transition';
+    
+    $: heroString = writable("Smith Hayward");
+
+    setTimeout(()=> {
+        $heroString = "Business Analyst";
+    }, 2000);
+
+</script>
+
+
+<div class="flex flex-row h-svh bg-slate-200">
+
+    <div transition:fly class=" text-nowrap mx-auto my-auto text-4xl">{$heroString}</div>
+
+</div>
