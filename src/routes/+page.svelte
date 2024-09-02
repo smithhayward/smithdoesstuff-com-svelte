d3c1ad8c1ccc819ca93aafbd0f595389d9393340
Smith Hayward <smith.hayward@gmail.com>
2024-09-02 15:57:39 -0700

    added more stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,14 +7,22 @@
     $: heroString = writable("Smith Hayward");
 
     setTimeout(()=> {
+        $heroString = "Data Wrangler";
+    }, 1500);
+
+    setTimeout(()=> {
+        $heroString = "Process Pragmatist";
+    }, 3000);
+
+    setTimeout(()=> {
         $heroString = "Business Analyst";
-    }, 2000);
+    }, 4500);
 
 </script>
 
 
 <div class="flex flex-row h-svh bg-slate-200">
-
-    <div transition:fade={{duration:800}} class=" text-nowrap mx-auto my-auto text-4xl">{$heroString}</div>
-
+{#key $heroString}
+    <div class=" text-nowrap mx-auto my-auto text-4xl">{$heroString}</div>
+{/key}
 </div>
